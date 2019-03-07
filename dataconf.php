6ee43f253eeb2d5199198287b4863b143e531671
Engjell Bislimi <engjellbislimi1@gmail.com>
2019-03-07 13:27:34 -0500

    Update dataconf.php

--- dataconf.php
+++ dataconf.php
@@ -2,11 +2,11 @@
 	// Remplir et renommer dataconf.php
 	//define("BASEURL", "http://127.0.0.1:8080/vino/");
 	// define("BASEURL", "   http://127.0.0.1/vino/");
-    define("BASEURL", "https://e1365422.webdev.cmaisonneuve.qc.ca/vino/");
-	define('HOST', 'localhost');
-	define('USER', 'e1365422');
-	define('PASSWORD', 'oHVCJECcOhQKlrrfhe95');
-	define('DATABASE', 'e1365422');
+    define("BASEURL", "/");
+	define('HOST', '/);
+	define('USER', '/');
+	define('PASSWORD', '/');
+	define('DATABASE', '/');
 	
 
 
