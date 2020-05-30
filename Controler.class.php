d2cc50650f9abea281700e4b6e28adbbfd60e11d
Engjell Bislimi <engjellbislimi1@gmail.com>
2020-05-29 23:07:50 -0400

    Update Controler.class.php

--- Controler.class.php
+++ Controler.class.php
@@ -2,8 +2,6 @@
 /**
  * Class Controler
  * Gère les requêtes HTTP
- * 
- * @author Jonathan Martel
  * @version 1.0
  * @update 2019-01-21
  * @license Creative Commons BY-NC 3.0 (Licence Creative Commons Attribution - Pas d’utilisation commerciale 3.0 non transposé)
